95d25d062e236a870038d153fc609ff98e05938d
strick <strickyak@users.noreply.github.com>
2015-08-08 17:52:29 -0700

    fizz buzz. gosub & return.

--- basic.go
+++ basic.go
@@ -1,14 +1,11 @@
 /*
-10 let n = 1000
-20 for i = 1 to n
-30 print i,
-40 next i
-50 stop
+  Old School BASIC.  Only numbers, so far.
 */
 package basic
 
 import (
 	`fmt`
+	//`os`
 	`regexp`
 	`sort`
 	`strconv`
@@ -16,15 +13,16 @@ import (
 )
 
 var Debug bool
+var Epsilon = 0.000001
 
 var F = fmt.Sprintf
 
-var FindKeyword = regexp.MustCompile(`^(rem|let|print|if|then|else|for|next|stop)\b`).FindString
+var FindKeyword = regexp.MustCompile(`^(?i)(rem|let|dim|print|goto|gosub|return|if|then|else|for|to|next|stop)\b`).FindString
 var FindNewline = regexp.MustCompile("^[;\n]").FindString
 var FindWhite = regexp.MustCompile("^[ \t]*").FindString
 var FindVar = regexp.MustCompile(`^[A-Za-z_][A-Za-z0-9_*]*`).FindString
-var FindNumber = regexp.MustCompile(`^[0-9]+`).FindString
-var FindOp = regexp.MustCompile(`^[-+*/<=>]{1,2}`).FindString
+var FindNumber = regexp.MustCompile(`^-?[0-9]+[.]?[0-9]*`).FindString
+var FindOp = regexp.MustCompile(`^[^A-Za-z0-9;\s]+`).FindString
 
 type Kind int
 
@@ -56,12 +54,6 @@ func (k Kind) String() string {
 	}
 }
 
-type ForFrame struct {
-	Var   string
-	Value float64
-	Max   float64
-}
-
 type Line struct {
 	N   int
 	Cmd Cmd
@@ -80,8 +72,10 @@ type Terp struct {
 	F       float64
 	// Interpreter state.
 	G           map[string]float64
-	Stack       []*ForFrame
+	ForFrames   []*ForFrame
+	GosubFrames []*GosubFrame
 	Lines       LineSlice
+	Line        int
 	LastPrinted float64
 }
 
@@ -90,28 +84,44 @@ func NewTerp(program string) *Terp {
 		Program: program,
 		G:       make(map[string]float64),
 	}
+	println(F("NewTerp-- P=%d K=%v S=%s", t.P, t.K, t.S))
+	t.Advance()
+	println(F("NewTerp-- P=%d K=%v S=%s", t.P, t.K, t.S))
 	t.Lines = t.ParseProgram()
+	println(F("NewTerp-- P=%d K=%v S=%s", t.P, t.K, t.S))
 	sort.Sort(t.Lines)
 	return t
 }
 
 func (t *Terp) Run() {
 	t.G = make(map[string]float64)
-	t.Stack = make([]*ForFrame, 0)
+	t.ForFrames = make([]*ForFrame, 0)
 	i := 0
 	n := len(t.Lines)
+Loop:
 	for i < n {
+		t.Line = t.Lines[i].N
+		println(F("%d: eval<< %v", i, t.Lines[i]))
 		j := t.Lines[i].Cmd.Eval(t)
+		println(F("%d: eval>>%d", i, j))
+
 		if j > 0 {
-			for i := 0; i < n; i++ {
-				if t.Lines[i].N == j {
-					break
+			// Branching instruction.
+			for i = 0; i < n; i++ {
+				println(F("%d: look got %d want %d", i, t.Lines[i].N, j))
+				if t.Lines[i].N >= j {
+					println(F("%d: CONTINUE got %d want %d", i, t.Lines[i].N, j))
+					continue Loop
 				}
 			}
+			println(F("FALLOUT"))
 		} else {
+			// Proceed to next instrution.
 			i++
+			println(F("NEXT"))
 		}
 	}
+	println(F("END"))
 }
 
 func (o *Terp) Advance() {
@@ -164,7 +174,7 @@ func (o *Terp) Advance1() {
 		o.P += len(m)
 		return
 	}
-	panic(F("Cannot Lex: 5q", o.Program[o.P:]))
+	panic(F("Cannot Lex: %q", o.Program[o.P:]))
 }
 
 type Expr struct {
@@ -172,6 +182,16 @@ type Expr struct {
 	Var   *string
 	A, B  *Expr
 	Op    string
+}
+
+func (o *Expr) String() string {
+	if o.Const != nil {
+		return F("(Const:%g)", o.Const)
+	}
+	if o.Var != nil {
+		return F("(Var:%g)", o.Const)
+	}
+	return F("(%s %s %s)", o.A.String(), Op, o.B.String())
 }
 
 func (o *Expr) Eval(t *Terp) float64 {
@@ -186,6 +206,16 @@ func (o *Expr) Eval(t *Terp) float64 {
 		switch o.Op {
 		case "+":
 			return a + b
+		case "-":
+			return a - b
+		case "*":
+			return a * b
+		case "/":
+			return a / b
+		case "%":
+			return float64(TrimInt(a) % TrimInt(b))
+		case "==":
+			return Truth(a == b)
 		default:
 			panic(F("bad op: %q", o.Op))
 		}
@@ -194,18 +224,68 @@ func (o *Expr) Eval(t *Terp) float64 {
 	}
 }
 
+func TrimInt(f float64) int {
+	println(F("TrimInt <<< %g", f))
+	h := 0.5
+	if f < 0 {
+		h = -0.5
+	}
+	i := int(f + h)
+	d := f - float64(i)
+	println(F("TrimInt >>> %i +- %f", i, d))
+	if d < -Epsilon || d > Epsilon {
+		panic(F("Not an integer: %g", f))
+	}
+	return i
+}
+
 type NopCmd struct{}
 
-func (o *NopCmd) Show(t *Terp) string { return "Nop..." }
+func (o *NopCmd) String() string { return "Nop..." }
 func (o *NopCmd) Eval(t *Terp) int {
 	return 0
+}
+
+type GosubFrame struct {
+	ReturnLine int
+}
+type GosubCmd struct {
+	CallLine int
+}
+
+func (o *GosubCmd) String() string { return F("Gosub %d", o.CallLine) }
+func (o *GosubCmd) Eval(t *Terp) int {
+	fr := &GosubFrame{t.Line + 1} // Return Address.
+	t.GosubFrames = append(t.GosubFrames, fr)
+	return o.CallLine // Call address.
+}
+
+type ReturnCmd struct {
+	N int
+}
+
+func (o *ReturnCmd) String() string { return F("Return") }
+func (o *ReturnCmd) Eval(t *Terp) int {
+	n := len(t.GosubFrames)
+	fr := t.GosubFrames[n-1]
+	t.GosubFrames = t.GosubFrames[:n-1]
+	return fr.ReturnLine
+}
+
+type GotoCmd struct {
+	N int
+}
+
+func (o *GotoCmd) String() string { return F("Goto %d", o.N) }
+func (o *GotoCmd) Eval(t *Terp) int {
+	return o.N
 }
 
 type PrintCmd struct {
 	X *Expr
 }
 
-func (o *PrintCmd) Show(t *Terp) string { return "Print..." }
+func (o *PrintCmd) String() string { return F("Print %v", o.X) }
 func (o *PrintCmd) Eval(t *Terp) int {
 	t.LastPrinted = o.X.Eval(t)
 	if Debug {
@@ -221,26 +301,84 @@ type LetCmd struct {
 }
 
 func (o *LetCmd) Eval(t *Terp) int {
-	z := o.X.Eval(t)
+	x := o.X.Eval(t)
 	if Debug {
-		println(F("## LET %s = %g", o.Var, z))
+		println(F("## LET %s = %g", o.Var, x))
 	}
-	t.G[o.Var] = z
+	t.G[o.Var] = x
 	return 0
 }
-func (o *LetCmd) Show(t *Terp) string { return "Let..." }
+func (o *LetCmd) String() string { return F("LET %s = %v", o.Var, o.X) }
 
-type ForCmd struct {
+type ForFrame struct {
 	Var   string
-	Value *Expr
-	Max   *Expr
+	Value float64
+	Max   float64
+	Line  int
 }
 type NextCmd struct {
 	Var string
 }
+
+func (o *NextCmd) String() string { return F("NEXT %s", o.Var) }
+func (o *NextCmd) Eval(t *Terp) int {
+	var i int
+	for i = len(t.ForFrames) - 1; i >= 0; i-- {
+		if t.ForFrames[i].Var == o.Var {
+			break
+		}
+	}
+	if i < 0 {
+		panic(F("NEXT %s: cannot find FOR frame", o.Var))
+	}
+	fr := t.ForFrames[i]
+	fr.Value += 1
+	t.G[o.Var] = fr.Value
+	if fr.Value > fr.Max {
+		t.ForFrames = t.ForFrames[:i]
+		return 0
+	}
+	t.ForFrames = t.ForFrames[:i+1]
+	return fr.Line
+}
+
+type ForCmd struct {
+	Var   string
+	Begin *Expr
+	End   *Expr
+}
+
+func (o *ForCmd) String() string { return F("FOR %s = %v TO %v", o.Var, o.Begin, o.End) }
+func (o *ForCmd) Eval(t *Terp) int {
+	begin := o.Begin.Eval(t)
+	end := o.End.Eval(t)
+	if Debug {
+		println(F("## FOR %s = %g TO %g", o.Var, begin, end))
+	}
+	t.G[o.Var] = begin
+	fr := &ForFrame{Var: o.Var, Value: begin, Max: end, Line: t.Line + 1}
+	t.ForFrames = append(t.ForFrames, fr)
+	return 0
+}
+
+type IfCmd struct {
+	Cond *Expr
+	Then int
+	Else int
+}
+
+func (o *IfCmd) String() string { return F("IF %v THEN %d ELSE %d", o.Cond, o.Then, o.Else) }
+func (o *IfCmd) Eval(t *Terp) int {
+	cond := o.Cond.Eval(t)
+	if TrimInt(cond) == 0 {
+		return o.Else
+	}
+	return o.Then
+}
+
 type Cmd interface {
 	Eval(t *Terp) int
-	Show(t *Terp) string
+	String() string
 }
 
 func (lex *Terp) ParsePrim() *Expr {
@@ -277,7 +415,10 @@ func (lex *Terp) ParseProgram() []Line {
 	var z []Line
 Loop:
 	for {
-		lex.Advance()
+		for lex.K == Newline {
+			lex.Advance()
+		}
+		println(F("LOOP %d %q", lex.P, lex.Program[lex.P:]))
 		// Want line number.
 		switch lex.K {
 		case EOF:
@@ -289,17 +430,19 @@ Loop:
 		default:
 			panic("expected line number")
 		}
+		println(F("LINE %d", n))
 		lex.Advance()
 		// Want command.
 		switch lex.K {
 		case Keyword:
 			w = lex.S
+			lex.Advance()
 		case Newline:
 			w = ";"
 		default:
-			panic("expected command")
+			panic("expected command after line number")
 		}
-		lex.Advance()
+		println(F("KEYWORD %s", w))
 		switch strings.ToLower(w) {
 		case ";":
 			c = &NopCmd{}
@@ -308,23 +451,81 @@ Loop:
 			for lex.K != EOF && lex.K != Newline {
 				lex.Advance()
 			}
+		case "goto":
+			n := int(lex.ParseNumber())
+			c = &GotoCmd{N: n}
+		case "gosub":
+			n := int(lex.ParseNumber())
+			c = &GosubCmd{CallLine: n}
+		case "return":
+			c = &ReturnCmd{}
 		case "print":
 			x := lex.ParseExpr()
 			c = &PrintCmd{X: x}
 		case "let":
-			Check(lex.K == Var, "expected var after let")
-			v := lex.S
-			lex.Advance()
-			Check(lex.K == Op && lex.S == "=", "expected = after var after let")
-			lex.Advance()
+			v := lex.ParseVar()
+			lex.ParseMustSym("=")
 			x := lex.ParseExpr()
 			c = &LetCmd{Var: v, X: x}
+		case "next":
+			v := lex.ParseVar()
+			c = &NextCmd{Var: v}
+		case "for":
+			v := lex.ParseVar()
+			lex.ParseMustSym("=")
+			x := lex.ParseExpr()
+			lex.ParseMustKeyword("to")
+			y := lex.ParseExpr()
+			c = &ForCmd{Var: v, Begin: x, End: y}
+		case "if":
+			cond := lex.ParseExpr()
+			lex.ParseMustKeyword("then")
+			ifTrue := TrimInt(lex.ParseNumber())
+			ifFalse := 0
+			if lex.S == "else" {
+				lex.ParseMustKeyword("else")
+				ifFalse = TrimInt(lex.ParseNumber())
+			}
+			c = &IfCmd{Cond: cond, Then: ifTrue, Else: ifFalse}
 		default:
 			panic("unknown command: " + w)
 		}
 		z = append(z, Line{N: n, Cmd: c})
+		if lex.K == EOF {
+			break
+		}
+		if lex.K != Newline {
+			panic("extra stuff at end of command")
+		}
 	}
 	return z
+}
+func (lex *Terp) ParseVar() string {
+	Check(lex.K == Var, "expected variable name")
+	s := lex.S
+	lex.Advance()
+	return s
+}
+func (lex *Terp) ParseNumber() float64 {
+	Check(lex.K == Number, "expected variable name")
+	f := lex.F
+	lex.Advance()
+	return f
+}
+func (lex *Terp) ParseMustSym(x string) {
+	Check(lex.K == Op && lex.S == x, "expected symbol: "+x)
+	lex.Advance()
+}
+func (lex *Terp) ParseMustKeyword(x string) {
+	Check(lex.K == Keyword && lex.S == x, "expected keyword: "+x)
+	lex.Advance()
+}
+
+func Truth(b bool) float64 {
+	if b {
+		return 1.0
+	}
+	return 0.0
 }
 
 func Check(b bool, s string) {
--- basic_test.go
+++ basic_test.go
@@ -7,11 +7,66 @@ import (
 	"testing"
 )
 
+type PF struct {
+	Prog  string
+	Final float64
+}
+
 func TestOne(t *testing.T) {
+	progs := []PF{
+		PF{`  10 let x = 42
+          20 print x + 1`, 43},
+		PF{`44 goto 88
+        55 print 13
+        88 print 99`, 99},
+		PF{`100 print 4 == 5`, 0},
+		PF{`100 print 4 == 4`, 1},
+		PF{`10 for i = 1 to 5
+        20 print i + 10
+        30 next i`, 15},
+		PF{`5 let s = 0;
+        10 for i = 1 to 10;
+        20 let s = s + i;
+        30 next i;
+        99 print s`, 55},
+		PF{`5 REM gosub and return
+        10 let x = 5
+        20 gosub 200
+        30 let x = x + 3
+        40 gosub 300
+        90 goto 9999
+        200 rem double the number
+        210 let x = x * 2
+        220 return
+        300 rem sextuple the number
+        310 let x = x * 3
+        320 gosub 200
+        330 return
+        9999 print x`, 78},
+		PF{`5 REM Fizz Buzz
+        10 for i = 1 to 100
+        20 if i % 15 == 0 then 400
+        30 if i % 3 == 0 then 300
+        40 if i % 5 == 0 then 500
+        50 print i
+        60 goto 900
+        300 print -3
+        310 goto 900
+        400 print -15
+        410 goto 900
+        500 print -5
+        510 goto 900
+        900 next i`, -5},
+	}
 	Debug = true
-	p1 := `10 let x = 42
-         20 print x + 1`
-	terp := NewTerp(p1)
-	terp.Run()
-	fmt.Printf("\n")
+	for i, pf := range progs {
+		t.Logf("Test Program %d: %q", i, pf.Prog)
+		terp := NewTerp(pf.Prog)
+		terp.Run()
+		fmt.Printf("\n")
+		final := terp.LastPrinted
+		if final != pf.Final {
+			t.Errorf("bad final: got %g want %g", final, pf.Final)
+		}
+	}
 }
