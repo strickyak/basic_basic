354bdaa0c1fe4b4699d061315431867262240804
strick <strickyak@users.noreply.github.com>
2015-08-09 19:20:09 -0700

    gofmt

--- web/main.go
+++ web/main.go
@@ -14,7 +14,7 @@ import (
 
 func main() {
 	flag.BoolVar(&Debug, "d", false, "debug bit")
-  flag.Parse()
+	flag.Parse()
 
 	http.HandleFunc("/", Render)
 	err := http.ListenAndServe(":12345", nil)
@@ -24,26 +24,26 @@ func main() {
 }
 
 func Render(w http.ResponseWriter, req *http.Request) {
-  defer func() {
-    r := recover()
-    if r != nil {
-      w.Header().Set("Content-Type", "text/plain")
-      Fprintf(w, "%v", r)
-    }
-  }()
+	defer func() {
+		r := recover()
+		if r != nil {
+			w.Header().Set("Content-Type", "text/plain")
+			Fprintf(w, "%v", r)
+		}
+	}()
 
-  var code string
+	var code string
 	req.ParseForm()
-  if x, ok := req.Form["code"]; ok {
-    code = x[0]
+	if x, ok := req.Form["code"]; ok {
+		code = x[0]
 
-    terp := NewTerp(code)
-    d := draw.Register(terp)
-    terp.Run()
-    Printf("\n")
-    w.Header().Set("Content-Type", "image/png")
-    if d.HasImage() {
-      d.WritePng(w)
-    }
-  }
+		terp := NewTerp(code)
+		d := draw.Register(terp)
+		terp.Run()
+		Printf("\n")
+		w.Header().Set("Content-Type", "image/png")
+		if d.HasImage() {
+			d.WritePng(w)
+		}
+	}
 }
